e798a2bf72dc0ed55d43413b1b0bcc818ffe68bb
s-s-3 <146006478+s-s-3@users.noreply.github.com>
2024-01-09 20:50:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,11 +36,6 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 resource "aws_ecs_task_definition" "app_task" {
   family                   = "app-first-task" # Name your task
   container_definitions    = <<DEFINITION
