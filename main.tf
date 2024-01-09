80b5c72dd5e0b1821e7d4472b1e7ab57ed6db496
s-s-3 <146006478+s-s-3@users.noreply.github.com>
2024-01-09 21:00:40 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ resource "aws_ecs_cluster" "my_cluster" {
   name = "app-cluster" # Name your cluster here
 }
 
+
 resource "aws_ecs_task_definition" "app_task" {
   family                   = "app-first-task" # Name your task
   container_definitions    = <<DEFINITION
@@ -43,6 +44,6 @@ resource "aws_ecs_task_definition" "app_task" {
   network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
   memory                   = 512         # Specify the memory the container requires
   cpu                      = 256         # Specify the CPU the container requires
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  execution_role_arn       = "arn:aws:iam::183074545480:role/ecsTaskExecutionRole"
 }
 
