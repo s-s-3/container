f7b9c08644eef66544f4ac1a5b1aa9d4fe4adb33
s-s-3 <146006478+s-s-3@users.noreply.github.com>
2024-01-09 21:11:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,3 +47,91 @@ resource "aws_ecs_task_definition" "app_task" {
   execution_role_arn       = "arn:aws:iam::183074545480:role/ecsTaskExecutionRole"
 }
 
+resource "aws_alb" "application_load_balancer" {
+  name               = "load-balancer-dev" #load balancer name
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "subnet-0ca2ba2af20245c77",
+    "subnet-0d0bec92cff62aed7"
+  ]
+  # security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Create a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "vpc-0115371ec8457f5a9" # default VPC
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" #  load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # target group
+  }
+}
+
+
+resource "aws_ecs_service" "app_service" {
+  name            = "app-first-service"     # Name the service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"   # Reference the created Cluster
+  task_definition = "${aws_ecs_task_definition.app_task.arn}" # Reference the task that the service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Set up the number of containers to 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Reference the target group
+    container_name   = "${aws_ecs_task_definition.app_task.family}"
+    container_port   = 5000 # Specify the container port
+  }
+
+  network_configuration {
+    subnets          = ["subnet-0ca2ba2af20245c77","subnet-0d0bec92cff62aed7"]
+    assign_public_ip = true     # Provide the containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
+  }
+}
+
+#Log the load balancer app URL
+output "app_url" {
+  value = aws_alb.application_load_balancer.dns_name
+}
