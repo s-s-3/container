def20bba39012fed569e48e86129ffd1f3a7088e
s-s-3 <146006478+s-s-3@users.noreply.github.com>
2024-01-09 20:02:12 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.45.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1" #The region where the environment 
+  #profile = "penguin"
+}
+
+resource "aws_ecr_repository" "app_ecr_repo" {
+  name = "app-repo"
+}
